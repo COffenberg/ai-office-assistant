592f09df1b293e3bf6f7795a945bc3d21d685360
Noah-Does-AltF4 <60921976+Noah-Does-AltF4@users.noreply.github.com>
2025-07-18 16:44:46 +0200

    Delete supabase directory
    Signed-off-by: Noah-Does-AltF4 <60921976+Noah-Does-AltF4@users.noreply.github.com>

--- supabase/migrations/20250626201210-929abf90-a9ac-4bff-8a35-8149c522e355.sql
+++
@@ -1,4 +0,0 @@
-
--- Clean up the accepted invitation for co@optivo.dk since the user already exists in profiles
-DELETE FROM public.invitations 
-WHERE email = 'co@optivo.dk' AND status = 'accepted';
